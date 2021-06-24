2124d3b15b7c0f1e8e242d44ea7228af852be2b4
ericrodriguesfer <er601432@gmail.com>
2021-06-24 11:17:55 -0300

    route admin - login finished

--- src/controllers/admin.js
+++ src/controllers/admin.js
@@ -19,22 +19,60 @@ module.exports = {
   },
 
   async create(request, response) {
-    const passHashed = encoder(request.body.pass);
+    Admin.find({ email: request.body.email })
+      .then((admin) => {
+        if (admin.length == 1) {
+          response.send({ message: "This email register other admin" });
+        } else if (admin.length <= 0) {
+          const passHashed = encoder(request.body.pass);
 
-    const newAdmin = {
-      name: request.body.name,
-      email: request.body.email,
-      login: request.body.login,
-      pass: passHashed,
-    };
+          const newAdmin = {
+            name: request.body.name,
+            email: request.body.email,
+            login: request.body.login,
+            pass: passHashed,
+          };
 
-    new Admin(newAdmin)
-      .save()
-      .then(() => {
-        response.send(newAdmin);
+          new Admin(newAdmin)
+            .save()
+            .then(() => {
+              response.send(newAdmin);
+            })
+            .catch((error) => {
+              response.send({ message: "Error in add new admin: " + error });
+            });
+        }
       })
       .catch((error) => {
-        response.send({ message: "Error in add new admin: " + error });
+        response.send({ message: "Fail in register admin: " + error });
+      });
+  },
+
+  async login(request, response) {
+    const passHashed = encoder(request.body.pass);
+
+    Admin.find({ login: request.body.login, pass: passHashed })
+      .then((admin) => {
+        if (admin.length == 1) {
+          const adminLoged = {
+            name: admin[0].name,
+            email: admin[0].email,
+            loged: true,
+            admin: true,
+          };
+          response.send(adminLoged);
+        } else if (admin.length <= 0) {
+          const adminNoLoged = {
+            name: "No loged",
+            email: "No loged",
+            loged: false,
+            admin: false,
+          };
+          response.send(adminNoLoged);
+        }
+      })
+      .catch((error) => {
+        response.send({ message: "Fail in login: " + error });
       });
   },
 };
--- src/routes/admin.js
+++ src/routes/admin.js
@@ -5,5 +5,6 @@ const admin = express.Router();
 
 admin.get("/", adminController.list);
 admin.post("/", adminController.create);
+admin.post("/login", adminController.login);
 
 module.exports = admin;
