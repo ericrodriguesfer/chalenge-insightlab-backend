a743f67af26c6f99a3dc8283b899fc2f1af1b1a1
ericrodriguesfer <er601432@gmail.com>
2021-06-27 12:33:45 -0300

    remove celebrate

--- src/core/core.js
+++ src/core/core.js
@@ -1,6 +1,5 @@
 const express = require("express");
 const cors = require("cors");
-const { errors } = require("celebrate");
 const connection = require("../config/connection");
 const routes = require("../routes/routes");
 
@@ -9,6 +8,5 @@ const core = express();
 core.use(cors());
 core.use(express.json());
 core.use(routes);
-core.use(errors());
 
 module.exports = core;
